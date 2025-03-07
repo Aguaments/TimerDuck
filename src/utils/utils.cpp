3bea283776f139cc48925b858f41f2167708c382
Aguaments <50241853+Aguaments@users.noreply.github.com>
2025-03-07 08:54:32 +0800

    Create utils.cpp

---
+++ src/utils/utils.cpp
@@ -0,0 +1,26 @@
+#include "tools.h"
+#include <iostream>
+#include <thread>
+#include <cmath>
+
+namespace ducktimer {
+
+    char tools::ibuffer[BUFFER_SIZE] = {0};
+    char tools::obuffer[BUFFER_SIZE] = {0};
+
+    void tools::clearCurrentLine(int len) {
+        int t = 1000;
+        while(len > 0) {
+            std::cout << "\b \b";
+            std::this_thread::sleep_for(std::chrono::milliseconds(t));
+            t = static_cast<int>(pow(t, 0.5));
+            len --;
+        }
+    }
+
+    void tools::setIONewBuffer(){
+        setvbuf(stdout, obuffer, _IONBF, sizeof(obuffer));
+        setvbuf(stdin, ibuffer, _IONBF, sizeof(ibuffer));
+    }
+
+}

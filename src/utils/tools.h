19293bb5a9f037ede3030027b794952d22f2dd49
Aguaments <50241853+Aguaments@users.noreply.github.com>
2025-03-07 08:54:04 +0800

    Create tools.h

---
+++ src/utils/tools.h
@@ -0,0 +1,16 @@
+#ifndef TOOLS_H
+#define TOOLS_H
+#define BUFFER_SIZE 4096
+
+namespace ducktimer {
+    class tools {
+    public:
+        static void clearCurrentLine(int len);
+        static void setIONewBuffer();
+    private:
+        static char ibuffer[BUFFER_SIZE];
+        static char obuffer[BUFFER_SIZE];
+    };
+}
+
+#endif //TOOLS_H

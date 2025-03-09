a6b562d200cf9f517615a9a1bfc770f4ac42d6aa
Aguaments <runacrosstheocean@163.com>
2025-03-09 15:46:35 +0800

    update 2025-03-09

--- src/music/music.cpp
+++ src/music/music.cpp
@@ -1,5 +1,36 @@
 #include "music.h"
+#include <windows.h>
+#include <dshow.h>
+#pragma comment(lib, "strmiids.lib")
 
-void ducktimer::play() const{
+namespace ducktimer{
+    void music::play() const{
+        CoInitialize(nullptr);
     
-}
+        IGraphBuilder* pGraph = nullptr;
+        IMediaControl* pControl = nullptr;
+        IMediaEvent* pEvent = nullptr;
+    
+        // 创建 Filter Graph
+        CoCreateInstance(CLSID_FilterGraph, nullptr, CLSCTX_INPROC_SERVER, IID_IGraphBuilder, (void**)&pGraph);
+        pGraph->QueryInterface(IID_IMediaControl, (void**)&pControl);
+        pGraph->QueryInterface(IID_IMediaEvent, (void**)&pEvent);
+    
+        // 加载 MP3 文件
+        pGraph->RenderFile(filename, nullptr);
+    
+        // 开始播放
+        pControl->Run();
+    
+        // 等待播放结束
+        long evCode;
+        pEvent->WaitForCompletion(INFINITE, &evCode);
+    
+        // 释放资源
+        pControl->Release();
+        pEvent->Release();
+        pGraph->Release();
+        CoUninitialize();
+    
+    }
+};

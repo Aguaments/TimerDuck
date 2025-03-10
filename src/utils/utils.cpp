21112ca225cb1fdf545943e979f89b341372a902
Aguaments <50241853+Aguaments@users.noreply.github.com>
2025-03-10 18:02:38 +0800

    Update utils.cpp

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -2,14 +2,32 @@
 #include <iostream>
 #include <thread>
 #include <cmath>
+#include <mutex>
+
+#define POINT_NUM 3
+#define BASE_INTERVAL 1000
 
 namespace ducktimer {
 
-    char tools::ibuffer[BUFFER_SIZE] = {0};
-    char tools::obuffer[BUFFER_SIZE] = {0};
+    std::unique_ptr<utils> utils::instance = nullptr;
+    std::mutex utils::mtx;
+    int utils::m_point_num = POINT_NUM;
 
-    void tools::clearCurrentLine(int len) {
-        int t = 1000;
+    utils& utils::getInstance() {
+        std::lock_guard<std::mutex> lock(mtx);
+        if(instance == nullptr) {
+            auto new_instance = new utils();
+            std::lock_guard<std::mutex> innerlock(mtx);
+            if(!instance) {
+                instance.reset(new_instance);
+            }
+        }
+        return *instance;
+    }
+
+
+    void utils::clearCurrentLine(int len) {
+        int t = BASE_INTERVAL;
         while(len > 0) {
             std::cout << "\b \b";
             std::this_thread::sleep_for(std::chrono::milliseconds(t));
@@ -18,9 +36,18 @@ namespace ducktimer {
         }
     }
 
-    void tools::setIONewBuffer(){
-        setvbuf(stdout, obuffer, _IONBF, sizeof(obuffer));
-        setvbuf(stdin, ibuffer, _IONBF, sizeof(ibuffer));
+    void utils::printPoint() {
+        for(int i = 0; i < m_point_num; ++ i) {
+            std::cout << ".";
+            std::this_thread::sleep_for(std::chrono::milliseconds(i == 2 ? 1500 : 1000));
+        }
+        auto space = std::string(" ", m_point_num);
+        auto back = std::string("\b", m_point_num);
+        std::cout << back << space <<back; // 褪掉打印出来的“.”
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     }
 
+    void utils::setPointNums(const int &nums) {
+        m_point_num = nums;
+    }
 }

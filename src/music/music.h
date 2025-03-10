ba0315cbc4bb4916ee911e7c595dcb24adfe1dfa
Aguaments <50241853+Aguaments@users.noreply.github.com>
2025-03-10 18:01:25 +0800

    Update music.h

--- src/music/music.h
+++ src/music/music.h
@@ -1,14 +1,20 @@
 #ifndef MUSIC_H
 #define MUSIC_H
+#include <atomic>
 #include <string>
+#include <utility>
 #include <windows.h>
 namespace ducktimer{
     class music{
     public:
-        explicit music(std::string fname):filename(fname){};
-        void playmusic() const;
+        explicit music(std::string fname):filename(std::move(fname)){};
+        void run() const;
+    private:
+        void mointerKeyboard() const;
+        void playMusic() const;
     private:
         std::string filename;
+        static std::atomic<bool> running;
     };
 }
 #endif

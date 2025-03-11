181709fd7d0649d90c7d28e5cea1649b1accfac4
Aguaments <50241853+Aguaments@users.noreply.github.com>
2025-03-11 08:44:22 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "timer.h"
 
 int main() {
-    ducktimer::timer t(30, "test2.mp3"); // Default setting: 30 minutes
+    ducktimer::timer t(30, "test1.mp3"); // Default setting: 30 minutes
     t.run();
     return 0;
 }
